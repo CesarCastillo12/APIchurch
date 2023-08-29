492437c8423ef2a546963d922928b4ddbdc30c43
cesar <cesar.cesaroronacastillo@gmail.com>
2023-08-29 15:26:30 -0600

    registro y login

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ mongoose.connect('mongodb+srv://cesar:123@cluster0.viwhkiu.mongodb.net/dbiglesia
 })
   .then(() => console.log('Conexión a MongoDB exitosa'))
   .catch(err => console.error('Error de conexión a MongoDB:', err));
+
 app.use(cors());
+app.use(bodyParser.json());
 
 const Usuario = mongoose.model('Usuario', {
   nombre: String,
@@ -18,16 +20,16 @@ const Usuario = mongoose.model('Usuario', {
   contraseña: String
 });
 
-app.use(bodyParser.json());
-
 app.post('/registro', async (req, res) => {
-  const nombreUsuarioExistente = await Usuario.findOne({ nombreUsuario: req.body.nombreUsuario });
-  const nombreExistente = await Usuario.findOne({ nombre: req.body.nombre });
+  const { nombre, nombreUsuario } = req.body;
+
+  const nombreUsuarioExistente = await Usuario.findOne({ nombreUsuario });
+  const nombreExistente = await Usuario.findOne({ nombre });
 
   if (nombreUsuarioExistente || nombreExistente) {
     if (
-      (nombreUsuarioExistente && nombreUsuarioExistente.nombre === req.body.nombre) ||
-      (nombreExistente && nombreExistente.nombreUsuario === req.body.nombreUsuario)
+      (nombreUsuarioExistente && nombreUsuarioExistente.nombre === nombre) ||
+      (nombreExistente && nombreExistente.nombreUsuario === nombreUsuario)
     ) {
       return res.status(409).json({ error: 'El nombre de usuario o el nombre ya existen' });
     }
@@ -37,6 +39,20 @@ app.post('/registro', async (req, res) => {
   nuevoUsuario.save()
     .then(() => res.status(201).json({ message: 'Usuario registrado con éxito' }))
     .catch(err => res.status(400).json({ error: 'Error al registrar usuario: ' + err }));
+});
+
+app.post('/login', async (req, res) => {
+  const { nombreUsuario, contraseña } = req.body;
+  try {
+    const usuario = await Usuario.findOne({ nombreUsuario, contraseña });
+    if (usuario) {
+      res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    } else {
+      res.status(401).json({ error: 'Credenciales incorrectas' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
 });
 
 const puerto = 3000;
