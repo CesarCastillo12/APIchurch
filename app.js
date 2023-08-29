2c1ae85d64a683cf19691182061472cfaf1f4a3b
cesar <cesar.cesaroronacastillo@gmail.com>
2023-08-29 14:59:07 -0600

    se arreglo el de verificar usuario

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ mongoose.connect('mongodb+srv://cesar:123@cluster0.viwhkiu.mongodb.net/dbiglesia
 })
   .then(() => console.log('Conexión a MongoDB exitosa'))
   .catch(err => console.error('Error de conexión a MongoDB:', err));
-
 app.use(cors());
-app.use(bodyParser.json());
 
 const Usuario = mongoose.model('Usuario', {
   nombre: String,
@@ -20,25 +18,25 @@ const Usuario = mongoose.model('Usuario', {
   contraseña: String
 });
 
-app.post('/registro', (req, res) => {
+app.use(bodyParser.json());
+
+app.post('/registro', async (req, res) => {
+  const nombreUsuarioExistente = await Usuario.findOne({ nombreUsuario: req.body.nombreUsuario });
+  const nombreExistente = await Usuario.findOne({ nombre: req.body.nombre });
+
+  if (nombreUsuarioExistente || nombreExistente) {
+    if (
+      (nombreUsuarioExistente && nombreUsuarioExistente.nombre === req.body.nombre) ||
+      (nombreExistente && nombreExistente.nombreUsuario === req.body.nombreUsuario)
+    ) {
+      return res.status(409).json({ error: 'El nombre de usuario o el nombre ya existen' });
+    }
+  }
+
   const nuevoUsuario = new Usuario(req.body);
   nuevoUsuario.save()
     .then(() => res.status(201).json({ message: 'Usuario registrado con éxito' }))
     .catch(err => res.status(400).json({ error: 'Error al registrar usuario: ' + err }));
-});
-
-app.post('/login', async (req, res) => {
-  const { nombreUsuario, contraseña } = req.body;
-  try {
-    const usuario = await Usuario.findOne({ nombreUsuario, contraseña });
-    if (usuario) {
-      res.status(200).json({ message: 'Inicio de sesión exitoso' });
-    } else {
-      res.status(401).json({ error: 'Credenciales incorrectas' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Error en el servidor' });
-  }
 });
 
 const puerto = 3000;
